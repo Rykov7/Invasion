5d30b789d3d783b0d200425f682f9ef3ad980df5
Rykov7 <rykov7@protonmail.ch>
2022-02-08 06:54:43 +0300

    Starting the Game Project

--- alien_invasion.py
+++ alien_invasion.py
@@ -22,6 +22,7 @@ class Invasion:
             # Make the most recently drawn screen visible.
             pygame.display.flip()
 
+
 if __name__ == '__main__':
     # Make a game instance, and run the game.
     ai = Invasion()
