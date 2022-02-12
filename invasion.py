72d59b2c34aa66214d862b2c8baa133465e5d234
Rykov7 <rykov7@protonmail.ch>
2022-02-12 16:47:40 +0300

    410 Scoring

--- invasion.py
+++ invasion.py
@@ -70,6 +70,9 @@ class Invasion:
         """Start a new game when the player clicks Play."""
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.stats.game_active:
+            # Reset the game settings.
+            self.settings.initialize_dynamic_settings()
+            
             # Reset the game statistics.
             self.stats.reset_stats()
             self.stats.game_active = True
@@ -129,6 +132,7 @@ class Invasion:
             # Destroy existing bullets and create new fleet.
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
 
     def _update_stars(self):
         """ Create and update stars."""
